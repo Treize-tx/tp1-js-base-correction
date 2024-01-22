5abcb9c7408f7c54a8ea3f3df578173e25f1741b
Lelexxx <rruiz.alex@gmail.com>
2024-01-22 23:49:39 +0100

    Split en 2 méthodes pour rspecter 1 méthode = 1 action

--- src/html-article.js
+++ src/html-article.js
@@ -4,10 +4,17 @@
 function displayArticleCount() { //pour déclarer une fonction on utilise le mot clé function, suivi du nom puis des paramètres entre parenthèses
     const newsList = document.querySelector('#newsList');
     const articles = newsList.querySelectorAll('#newsList .article'); //retourne une liste d'élément HTML
+
+    updateArticleCount(articles.length)
+}
+
+function updateArticleCount(count){
     const h3 = document.querySelector('h3');
 
-    if (articles.length > 1)
+    if (count) {
         h3.innerText = `There are ${articles.length} articles`;
-    else
+    }
+    else {
         h3.innerText = `There is ${articles.length} article`;
+    }
 }
